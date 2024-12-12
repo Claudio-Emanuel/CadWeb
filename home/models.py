de84b9c1d6e2b183c6211757e631502eb2227023
Claudio-Emanuel <cemanuelcampos13@gmail.com>
2024-12-12 01:34:28 -0300

    Deu Certo 2 V1.9

---
+++ home/models.py
@@ -0,0 +1,9 @@
+import locale
+from django.db import models
+
+class Categoria(models.Model):
+    nome= models.CharField(max_length=100)
+    ordem= models.IntegerField()
+
+    def _str_(self):
+        return self.nome
