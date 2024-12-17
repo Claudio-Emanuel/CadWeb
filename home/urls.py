2efdde301c1383bed356b0d5184868a45357c91b
Claudio-Emanuel <cemanuelcampos13@gmail.com>
2024-12-17 02:07:42 -0300

    Deu Certo V3.0

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('categoria', views.categoria, name='categoria'),
-    path('formularioCategoria' , views.form_categoria, name='formulario')
+    path('formularioCategoria' , views.form_categoria, name='formulario'),
+    path('editar_categoria/<int:id>',views.editar_categoria, name='editar_categoria'),
+    path('delet_categoria/<int:id>',views.delet_categoria, name='delet_categoria'),
+    path('detail_categoria/<int:id>',views.detail_categoria, name='detail_categoria')
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-
+from django.contrib import messages
 from .models import *
 from .forms import *
 
@@ -21,6 +21,7 @@ def form_categoria(request):
 #--------------------------------------------------------    
         if form.is_valid():
             form.save()
+            messages.success(request, 'Registro realizado com sucesso!!')
             return redirect('categoria')
     else:
         form=CategoriaForm()
@@ -30,3 +31,50 @@ def form_categoria(request):
     }
     return render(request, 'formulario.html', contexto )
 #--------------------------------------------------------    
+
+#------------Funcao de Editar Categoria de Produto------------ 
+def editar_categoria(request, id):
+    try:
+        categoria = Categoria.objects.get(pk=id)
+    except Categoria.DoesNotExist:
+        messages.error(request, 'Não foi possível encontrar a categoria solicitada')
+        return redirect('categoria')
+        
+    if request.method == 'POST':
+        # combina os dados do formulário submetido com a instância do objeto existente, permitindo editar seus valores.
+        form = CategoriaForm(request.POST, instance=categoria)
+        if form.is_valid():
+            categoria = form.save() # save retorna o objeto salvo
+            lista = []
+            lista.append(categoria) 
+            return render(request, 'categoria.html', {'lista': lista})
+    else:
+         form = CategoriaForm(instance=categoria)
+    return render(request, 'formulario.html', {'form': form,})
+
+#------------Funcao de Deletar Categoria de Produto------------ 
+def delet_categoria(request,id):
+    try:
+        categoria = Categoria.objects.get(pk=id)
+        categoria.delete()
+        messages.info(request, 'Item deletado com sucesso!!!')
+
+    except Categoria.DoesNotExist:
+        messages.error(request, 'Não foi possível encontrar a categoria solicitada')
+        return redirect('categoria')
+    return redirect('categoria')
+
+#------------Funcao de Detalhar Categoria de Produto------------ 
+def detail_categoria(request,id):
+    try:
+        categoria = Categoria.objects.get(pk=id)
+    except Categoria.DoesNotExist:
+        messages.error(request, 'Não foi possível encontrar a categoria solicitada')
+        return redirect('categoria')
+        
+    if request.method == 'POST':
+        CategoriaForm(request.POST, instance=categoria)
+    else:
+        form = CategoriaForm(instance=categoria)
+    return render(request, 'detail.html', {'form': form,})
+    
